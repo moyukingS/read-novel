55315e2b46d4b4a4466cb1091d8f446a89dc5099
mini dev <>
2025-07-21 11:50:02 +0800

    feat: 更新Logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <n-config-provider :theme="store.isDark ? darkTheme : null">
     <n-layout :native-scrollbar="false" class="h-screen">
-      <HeaderBar />
+      <HeaderBar/>
       <n-drawer v-model:show="store.showManager" width="400" placement="right">
         <n-drawer-content>
           <template #header>
@@ -9,18 +9,18 @@
               小说管理
             </h3>
           </template>
-          <NovelManager class="mb-6" />
+          <NovelManager class="mb-6"/>
           <template #footer>
-            <UploadCard />
+            <UploadCard/>
           </template>
         </n-drawer-content>
       </n-drawer>
-      <n-layout has-sider style="height: calc(100vh - 45px);">
-        <Sidebar />
+      <n-layout has-sider :content-style="{'padding-top': '15px'}">
+        <Sidebar/>
         <n-layout-content>
-          <div style="height: 100%; display: flex; flex-direction: column;">
-            <UploadCard v-if="!store.uploaded" />
-            <ReaderContent v-else />
+          <div  class="h-full flex flex-col ">
+            <UploadCard v-if="!store.uploaded"/>
+            <ReaderContent v-else/>
           </div>
         </n-layout-content>
       </n-layout>
@@ -28,16 +28,17 @@
   </n-config-provider>
 </template>
 <script setup>
-import { darkTheme } from 'naive-ui'
+import {darkTheme} from 'naive-ui'
+
 const store = useNovelStore()
 
 onMounted(async () => {
   await store.loadNovels()
   if (
-    store.lastReadNovelName &&
-    typeof store.lastReadNovelName === 'object' &&
-    'value' in store.lastReadNovelName &&
-    store.lastReadNovelName.value
+      store.lastReadNovelName &&
+      typeof store.lastReadNovelName === 'object' &&
+      'value' in store.lastReadNovelName &&
+      store.lastReadNovelName.value
   ) {
     await store.selectNovel(store.lastReadNovelName.value)
   }
@@ -45,13 +46,13 @@ onMounted(async () => {
 
 // 自动设置网页 title
 watch(
-  () => [store.currentNovel, store.chaptersMeta, store.currentChapterIndex],
-  () => {
-    const novel = store.currentNovel || ''
-    const chapter = store.chaptersMeta?.[store.currentChapterIndex]?.title || ''
-    document.title = chapter ? `${novel} - ${chapter}` : novel
-  },
-  { immediate: true, deep: true }
+    () => [store.currentNovel, store.chaptersMeta, store.currentChapterIndex],
+    () => {
+      const novel = store.currentNovel || ''
+      const chapter = store.chaptersMeta?.[store.currentChapterIndex]?.title || ''
+      document.title = chapter ? `${novel} - ${chapter}` : novel
+    },
+    {immediate: true, deep: true}
 )
 </script>
 <style></style>
--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,10 +1,8 @@
 <template>
-  <n-layout-header>
+  <n-layout-header bordered>
     <div class="w-full flex justify-between items-center px-4 h-[45px]">
       <div class="flex items-center">
-        <n-icon size="24">
-          <BookOutline />
-        </n-icon>
+        <a href="/"><img :src="Logo" alt="logo" class="w-10 rounded "/></a>
         <span class="ml-2 font-bold text-lg">read-novel</span>
       </div>
       <div v-if="store.uploaded" class="flex items-center gap-4">
@@ -14,32 +12,32 @@
           </template>
           <template v-else>
             <n-input-group>
-              <n-input v-model:value="editName" size="small" style="width:120px" @keyup.enter="saveName" />
+              <n-input v-model:value="editName" size="small" style="width:120px" @keyup.enter="saveName"/>
               <n-button size="small" type="primary" @click="saveName">保存</n-button>
               <n-button size="small" @click="cancelEdit">取消</n-button>
             </n-input-group>
           </template>
         </div>
         <n-progress
-          type="circle"
-          :percentage="store.chaptersMeta.length ? ((store.currentChapterIndex + 1) / store.chaptersMeta.length) * 100 : 0"
-          :show-indicator="false"
-          
-          :stroke-width="8"
-          style="width: 26px; height: 26px;"
+            type="circle"
+            :percentage="store.chaptersMeta.length ? ((store.currentChapterIndex + 1) / store.chaptersMeta.length) * 100 : 0"
+            :show-indicator="false"
+
+            :stroke-width="8"
+            style="width: 26px; height: 26px;"
         >
         </n-progress>
       </div>
       <div class="flex gap-4 items-center">
         <n-button quaternary circle @click="store.toggleTheme">
           <n-icon>
-            <MoonOutline v-if="!store.isDark" />
-            <SunnyOutline v-else />
+            <MoonOutline v-if="!store.isDark"/>
+            <SunnyOutline v-else/>
           </n-icon>
         </n-button>
         <n-button quaternary circle @click="store.showManager = true">
           <n-icon>
-            <SettingsOutline />
+            <SettingsOutline/>
           </n-icon>
         </n-button>
       </div>
@@ -48,7 +46,9 @@
 </template>
 
 <script setup>
-import { BookOutline,MoonOutline, SunnyOutline, SettingsOutline } from '@vicons/ionicons5'
+import Logo from '@/assets/images/cat.svg'
+import {BookOutline, MoonOutline, SunnyOutline, SettingsOutline} from '@vicons/ionicons5'
+
 const store = useNovelStore()
 
 const editingName = ref(false)
@@ -64,6 +64,7 @@ async function saveName() {
   }
   editingName.value = false
 }
+
 function cancelEdit() {
   editName.value = store.currentNovel
   editingName.value = false
